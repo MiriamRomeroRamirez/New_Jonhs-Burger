5dbc829431d2e31a43be82c05d39e63098b7071c
CeciAmaro <amaro.ceci.10@gmail.com>
2022-08-09 13:30:40 -0500

    Avance api

---
+++ db.php
@@ -0,0 +1,15 @@
+<?php
+$servername="localhost:3307";
+$username="root";
+$password="";
+$dbname="bdmapas";
+
+$conn=new mysqli($servername,$username,$password,$dbname);
+
+if($conn->connect_error){
+	die("Connection Failed".$conn->connect_error);
+}else{
+	
+}
+
+?>
--- modelos/conexion.php
+++ modelos/conexion.php
@@ -4,8 +4,9 @@ class Conexion{
 
 	static public function conectar(){
 		
-		$link = new PDO("mysql:host=localhost:3306;dbname=multiservicio","root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+		$link = new PDO("mysql:host=localhost:3307;dbname=multiservicio","root","",array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
 		                      PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
 		return $link;
 	}
+
 }
